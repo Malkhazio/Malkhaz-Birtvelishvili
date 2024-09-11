8ebb54884adbf544f98ea96fa255a2e3d81537ac
Malkhazio <malkhaz.birtvelishvili@iset.ge>
2024-09-11 21:41:54 +0400

    Add files via upload

---
+++ TBC-Academy_task_4.py
@@ -0,0 +1,4 @@
+Temperature_in_Celcius = int(input("რა ტემპერატურაა ცელსიუსებში? "))
+Temperature_in_Fahrenheit = Temperature_in_Celcius * 1.8 + 32
+print(f"ტემპერატურა შეადგენს {Temperature_in_Fahrenheit} ფარენჰეიტს")
+
