419e4b78735d88ae0df144e2252ae08af586a23b
Malkhaz Birtvelishvili <malkhaz.birtvelishvili@iset.ge>
2024-09-18 18:14:09 +0400

    Delete Task_1.py

--- Task_1.py
+++
@@ -1,4 +0,0 @@
-number = float(input("შეიყვანე რიცხვი რისი ხარისხში აყვანაც გსურს: "))
-degree_of_the_number = float(input("შეიყვანე ხარისხის რიცხვი: "))
-result = math.pow(number, degree_of_the_number)
-print(result)
