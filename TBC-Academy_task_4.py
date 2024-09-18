07f33dc30750812910a15c19706d9941484a0b17
Malkhaz Birtvelishvili <malkhaz.birtvelishvili@iset.ge>
2024-09-18 18:13:53 +0400

    Delete TBC-Academy_task_4.py

--- TBC-Academy_task_4.py
+++
@@ -1,4 +0,0 @@
-Temperature_in_Celcius = int(input("რა ტემპერატურაა ცელსიუსებში? "))
-Temperature_in_Fahrenheit = Temperature_in_Celcius * 1.8 + 32
-print(f"ტემპერატურა შეადგენს {Temperature_in_Fahrenheit} ფარენჰეიტს")
-
