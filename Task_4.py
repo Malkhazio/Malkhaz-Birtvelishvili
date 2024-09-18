4644f7918f559ba2b7e7cf0d1e7cf7ffc7e100c9
Malkhaz Birtvelishvili <malkhaz.birtvelishvili@iset.ge>
2024-09-18 18:14:57 +0400

    Delete Task_4.py

--- Task_4.py
+++
@@ -1,4 +0,0 @@
-import random
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-print(random.choice(values), random.choice(suits))
